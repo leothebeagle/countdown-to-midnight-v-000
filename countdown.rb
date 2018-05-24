d169b91ba07dac04ee8968d5e85bb16b83bc3f89
Aziz Kosber <a.kosber@gmail.com>
2018-05-24 19:54:35 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,10 @@
 #write your code here
 
 def countdown(seconds)
-
+  seconds_left = seconds
+  until seconds_left == 0
+    puts "#{seconds_left} SECOND(S)"
+    seconds_left -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
